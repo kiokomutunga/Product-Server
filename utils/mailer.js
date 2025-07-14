ca9e3dd8ec0e2ebe4ba7c1adfa78e26fe0cdcebe
kiokomutunga <kiokomutungapp@gmail.com>
2025-07-14 20:06:33 +0300

    order management and product adding

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,3 +7,5 @@ const transporter = nodemailer.createTransport({
 
 exports.sendEmail = ({ to, subject, text, html }) =>
   transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text, html });
+console.log("Email user:", process.env.EMAIL_USER);
+console.log("Email pass:", process.env.EMAIL_PASS ? "Loaded" : "Not Loaded");
