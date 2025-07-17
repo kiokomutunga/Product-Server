4b900c2662f61a0a205272f90e5ab1fce9521f49
kiokomutunga <kiokomutungapp@gmail.com>
2025-07-18 00:41:45 +0300

    authroute2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,11 +7,12 @@ const {
   resetPassword,
   changePassword,
   getProfile,
+  registerAdmin,
   googleLogin
 } = require("../controllers/authController");
 
 const { authenticateUser } = require("../middleware/auth");
-
+router.post("/admin/register", registerAdmin);
 router.post("/register", register);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
@@ -20,6 +21,8 @@ router.post("/reset-password", resetPassword);
 router.post("/change-password", authenticateUser, changePassword);
 router.post("/google-login", googleLogin);
 router.get("/profile", authenticateUser, getProfile);
+
+
 
 
 module.exports = router;
