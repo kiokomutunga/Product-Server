701ddff43a137754a1efffd257bec81ff096d8b0
kiokomutunga <kiokomutungapp@gmail.com>
2025-07-20 00:10:41 +0300

    orders require admin

--- routes/orderroutes.js
+++ routes/orderroutes.js
@@ -15,20 +15,18 @@ router.post("/place",authenticateUser, placeOrder);
 
 //  Get user's own orders
 router.get("/user", authenticateUser, getUserOrders);
-//router.get("/user", getUserOrders);
 //  Admin fetches all orders
-//router.get("/admin", authenticateUser, requireAdmin, getAllOrders);
-//router.get("/admin", getAllOrders);
-router.get("/", getAllOrders); // in routes/orderRoutes.js
+router.get("/", authenticateUser, requireAdmin, getAllOrders);
+//router.get("/", getAllOrders); // in routes/orderRoutes.js
 
 
 // Admin updates order status (e.g., Processing → Shipped)
-router.put("/status/:id", authenticateUser, updateOrderStatus);
+router.put("/status/:id", authenticateUser, requireAdmin, updateOrderStatus);
 
 // Admin marks order as delivered (triggers delivery email)
-router.put("/deliver/:id", markOrderDelivered);
+router.put("/deliver/:id",requireAdmin, markOrderDelivered);
 
 // Admin cancels order
-router.put("/cancel/:id", authenticateUser, cancelOrder);
+router.put("/cancel/:id", authenticateUser,requireAdmin, cancelOrder);
 
 module.exports = router;
